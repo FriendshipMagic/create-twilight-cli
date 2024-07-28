391d9aca5561aeb4ad0ab700a01fea36143e5280
bloodymoon <1270046784@qq.com>
2024-07-28 22:52:17 +0800

    chore: 改用第三方库fs-extra实现目录的递归复制
    Signed-off-by: bloodymoon <1270046784@qq.com>

--- index.js
+++ index.js
@@ -2,28 +2,12 @@
 
 import path from 'path'
 import { fileURLToPath } from 'url'
-import { copyFileSync, existsSync, mkdirSync, readdirSync, statSync } from 'fs'
+import fs from 'fs-extra'
 import { execaSync } from 'execa'
 import inquirer from 'inquirer'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-
-function copyRecursiveSync(src, dest) {
-    const exists = existsSync(src)
-    const stats = exists && statSync(src)
-    const isDirectory = exists && stats.isDirectory()
-    if (isDirectory) {
-        if (!existsSync(dest)) {
-            mkdirSync(dest)
-        }
-        readdirSync(src).forEach(childItemName => {
-            copyRecursiveSync(path.join(src, childItemName), path.join(dest, childItemName))
-        })
-    } else {
-        copyFileSync(src, dest)
-    }
-}
 
 async function main() {
     const answers = await inquirer.prompt([
@@ -33,22 +17,21 @@ async function main() {
             message: 'Project name:'
         }
     ])
-
     const projectName = answers.name || 'my-app'
     const projectPath = path.join(process.cwd(), projectName)
 
-    if (!existsSync(projectPath)) {
-        mkdirSync(projectPath)
+    const { copySync, ensureDirSync, pathExistsSync } = fs
+    if (!pathExistsSync(projectPath)) {
+        ensureDirSync(projectPath)
     }
 
     const templatePath = path.join(__dirname, 'template')
-
-    copyRecursiveSync(templatePath, projectPath)
+    // 递归复制目录
+    copySync(templatePath, projectPath)
 
     console.log(`Creating a new project in ${projectPath}.`)
 
     execaSync('npm', ['install'], { cwd: projectPath, stdio: 'inherit' })
-
     console.log('Project setup complete!')
 }
 
