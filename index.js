531784180ea85d1fd4c54a6d0819a6dd0775c72b
bloodymoon <1270046784@qq.com>
2024-07-28 14:17:53 +0800

    fix: 修正导入方式

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 
 const { program } = require('commander')
-const execa = require('execa')
 const fs = require('fs-extra')
 const path = require('path')
 
-program.version('1.0.0').argument('<project-name>', 'Name of the new project').action((projectName) => {
+// 动态导入 execa 模块
+const importExeca = async () => {
+    return await import('execa')
+}
+
+program.version('1.0.0').argument('<project-name>', 'Name of the new project').action(async (projectName) => {
     const projectPath = path.join(process.cwd(), projectName)
     const templatePath = path.join(__dirname, 'template')
 
@@ -16,6 +20,7 @@ program.version('1.0.0').argument('<project-name>', 'Name of the new project').a
 
     fs.copySync(templatePath, projectPath)
 
+    const execa = await importExeca()
     execa.sync('npm', ['install'], { cwd: projectPath, stdio: 'inherit' })
 
     console.log(`Project "${projectName}" created successfully!`)
