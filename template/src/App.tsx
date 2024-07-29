0fcb6886441c78c00f26963c86045d00cbce6983
bloodymoon <1270046784@qq.com>
2024-07-29 23:43:26 +0800

    合并拉取请求 #4
    feat: 支持深色模式
    Signed-off-by: bloodymoon <1270046784@qq.com>

--- template/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import 'tailwindcss/tailwind.css'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './config/routes.tsx'
-
-export default function App() {
-    return <RouterProvider router={router} />
-}
