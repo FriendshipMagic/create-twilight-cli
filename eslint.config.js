98c8f069d83de48d666cdd4cf8ef07f97aa8bf53
bloodymoon <1270046784@qq.com>
2024-08-15 23:03:27 +0800

    feat: 支持对node.js的eslint检查

--- eslint.config.js
+++ eslint.config.js
@@ -4,20 +4,31 @@ import { fileURLToPath } from 'node:url'
 import js from '@eslint/js'
 import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// 获取当前文件路径
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+// 兼容性配置
 const compat = new FlatCompat({
     baseDirectory: __dirname,
     recommendedConfig: js.configs.recommended,
     allConfig: js.configs.all
-});
+})
 
+// 导出 ESLint 配置
 export default [{
-    ignores: ["**/dist", "**/template"],
-}, ...compat.extends("eslint:recommended"), {
-    languageOptions: {
-        globals: {
-            ...globals.browser,
-        },
-    },
-}];
+    ignores: ['**/dist', '**/template']
+},
+    ...compat.extends('eslint:recommended'),
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node
+            },
+            parserOptions: {
+                ecmaVersion: 2021,
+                sourceType: 'module'
+            }
+        }
+    }]
